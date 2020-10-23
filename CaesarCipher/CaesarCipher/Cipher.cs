e1b5144dc6966e0c95bcc32291bb8b45dc5b6f12
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-10-23 11:52:52 +0300

    Decryption realization

--- CaesarCipher/CaesarCipher/Cipher.cs
+++ CaesarCipher/CaesarCipher/Cipher.cs
@@ -42,9 +42,9 @@ namespace CaesarCipher
                 {
                     // calculate new ascii code:
                     // if new ascii code is larger than Z's ascii code,
-                    // I subtract new ascii code by Z's ascii value to know how much I
-                    // need to shift from A, and add that value to (A's code - 1) because
-                    // I don't want to ignore letter A
+                    // I add given letter's code to secret key to get new ascii code, then
+                    // subtract it by Z's ascii code and add the difference to ascii code one lower than A
+                    // so I wouldn't ignore letter A
                     // otherwise, new ascii code is new letter
                     newCode = (letterCode + secretKey) > (int)'Z' ? ((int)'A' - 1) + ((letterCode + secretKey) - (int)'Z') : letterCode + secretKey;
                 }
@@ -52,7 +52,6 @@ namespace CaesarCipher
                 {
                     // similar as above, just with lower case letters
                     newCode = (letterCode + secretKey) > (int)'z' ? ((int)'a' - 1) + ((letterCode + secretKey) - (int)'z') : letterCode + secretKey;
-                    
                 }
                 letters[i] = (char)newCode;
             }
@@ -66,7 +65,32 @@ namespace CaesarCipher
             // convert cipher to char array
             char[] letters = cipher.ToCharArray();
 
+            for (int i = 0; i < letters.Length; i++)
+            {
+                // take ascii code of letter
+                int letterCode = (int)letters[i];
 
+                // new ascii code
+                int newCode;
+
+                // check if letter is upper case
+                if (char.IsUpper(letters[i]))
+                {
+                    // calculate new ascii code:
+                    // if new ascii code is smaller than A's ascii code,
+                    // I subtract from A's ascii code the difference of given letter's code to secret key
+                    // and subtract that from ascii code which is one larger than Z's code
+                    // so I wouldn't ignore letter Z
+                    // otherwise, new ascii code is new letter
+                    newCode = (letterCode - secretKey) < (int)'A' ? ((int)'Z' + 1) - ((int)'A' - (letterCode - secretKey)) : letterCode - secretKey;
+                }
+                else
+                {
+                    // similar as above, just with lower case letters
+                    newCode = (letterCode - secretKey) < (int)'a' ? ((int)'z' + 1) - ((int)'a' - (letterCode - secretKey)) : letterCode - secretKey;
+                }
+                letters[i] = (char)newCode;
+            }
 
             // return plain text
             return new string(letters);
