a41b1f45c4be43482930978fe4781961b2b4ef62
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-10-23 11:05:55 +0300

    Encryption realization

--- CaesarCipher/CaesarCipher/Cipher.cs
+++ CaesarCipher/CaesarCipher/Cipher.cs
@@ -13,17 +13,44 @@ namespace CaesarCipher
 
         public Cipher(int secretKey)
         {
+
+            // if secret key is larger than number of letters in alphabet,
+            // we take the remainder
+            if (secretKey >= letterCountInAlphabet)
+            {
+                secretKey = secretKey % letterCountInAlphabet;
+            }
+
             this.secretKey = secretKey;
         }
 
         public string Encrypt(string plainText)
         {
-            return "";
+            // convert plainText to char array
+            char[] letters = plainText.ToCharArray();
+
+            // shift every letter
+            for (int i = 0; i < letters.Length; i++)
+            {
+                // take ascii code of letter
+                int letterCode = (int)letters[i];
+
+                // calculate new ascii code:
+                // if new ascii code is larger than z's ascii code,
+                // I subtract new ascii code by z's ascii value to know how much I
+                // need to shift from a, and add that value to (a's code - 1) because
+                // I don't want to ignore letter a
+                // otherwise, new ascii code is new letter
+                int newCode = (letterCode + secretKey) > (int)'z' ? ((int)'a' - 1) + ((letterCode + secretKey) - (int)'z') : letterCode + secretKey;
+                letters[i] = (char)newCode;
+            }
+
+            // return cipher
+            return new string(letters);
         }
 
         public string Decrypt(string cipher)
         {
-            return "";
         }
     }
 }
