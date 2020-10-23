ddb3b2cd2ef7f83320781685bc335f3055b2beb6
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-10-23 10:26:52 +0300

    Added fields and stub methods to Cipher class

--- CaesarCipher/CaesarCipher/Cipher.cs
+++ CaesarCipher/CaesarCipher/Cipher.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CaesarCipher
 {
-    class Cipher
+    public class Cipher
     {
+        private readonly int secretKey; // cannot change secret key after creating object
+        private const int letterCountInAlphabet = (int)'z' - (int)'a' + 1;
+
+        public Cipher(int secretKey)
+        {
+            this.secretKey = secretKey;
+        }
+
+        public string Encrypt(string plainText)
+        {
+            return "";
+        }
+
+        public string Decrypt(string cipher)
+        {
+            return "";
+        }
     }
 }
