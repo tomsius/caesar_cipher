184b342b46cc26d49047c26fe789234910f8ebf7
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-22 10:20:59 +0200

    Refactoring

--- CaesarCipher/CaesarCipher/Cipher.cs
+++ CaesarCipher/CaesarCipher/Cipher.cs
@@ -8,16 +8,14 @@ namespace CaesarCipher
 {
     public class Cipher
     {
-        private readonly int secretKey; // cannot change secret key after creating object
-        private const int letterCountInAlphabet = (int)'z' - (int)'a' + 1;
+        private readonly int secretKey;
+        private const int letterCountInAlphabet = 'z' - 'a' + 1;
 
         public Cipher(int secretKey)
         {
-            // if secret key is larger than number of letters in alphabet,
-            // we take the remainder
             if (secretKey >= letterCountInAlphabet)
             {
-                secretKey = secretKey % letterCountInAlphabet;
+                secretKey %= letterCountInAlphabet;
             }
 
             this.secretKey = secretKey;
@@ -25,101 +23,89 @@ namespace CaesarCipher
 
         public string Encrypt(string plainText)
         {
-            // if secret key is divisible by letter count in alphabet,
-            // then there is no shift
             if (secretKey == 0)
             {
                 return plainText;
             }
 
-            // convert plainText to char array
             char[] letters = plainText.ToCharArray();
 
-            // shift every letter
             for (int i = 0; i < letters.Length; i++)
             {
-                // ignore if it's not a letter
-                if (!char.IsLetter(letters[i]))
+                if (IsNotLetter(letters[i]))
                 {
                     continue;
                 }
 
-                // take ascii code of letter
-                int letterCode = (int)letters[i];
-
-                // new ascii code
-                int newCode;
-
-                // check if letter is upper case
-                if (char.IsUpper(letters[i]))
-                {
-                    // calculate new ascii code:
-                    // if new ascii code is larger than Z's ascii code,
-                    // I add given letter's code to secret key to get new ascii code, then
-                    // subtract it by Z's ascii code and add the difference to ascii code one lower than A
-                    // so I wouldn't ignore letter A
-                    // otherwise, new ascii code is new letter
-                    newCode = (letterCode + secretKey) > (int)'Z' ? ((int)'A' - 1) + ((letterCode + secretKey) - (int)'Z') : letterCode + secretKey;
-                }
-                else
-                {
-                    // similar as above, just with lower case letters
-                    newCode = (letterCode + secretKey) > (int)'z' ? ((int)'a' - 1) + ((letterCode + secretKey) - (int)'z') : letterCode + secretKey;
-                }
-                letters[i] = (char)newCode;
+                letters[i] = (char)EncryptLetter(letters[i]);
             }
 
-            // return cipher
             return new string(letters);
+        }
+
+        private bool IsNotLetter(char letter)
+        {
+            return !char.IsLetter(letter);
+        }
+
+        private int EncryptLetter(char letter)
+        {
+            if (char.IsUpper(letter))
+            {
+                // calculate new ascii code:
+                // if new ascii code is larger than Z's ascii code,
+                // I add given letter's code to secret key to get new ascii code, then
+                // subtract it by Z's ascii code and add the difference to ascii code one lower than A
+                // so I wouldn't ignore letter A
+                // otherwise, new ascii code is new letter
+                return (letter + secretKey) > 'Z' ? ('A' - 1) + ((letter + secretKey) - 'Z') : letter + secretKey;
+            }
+            else
+            {
+                // similar as above, just with lower case letters
+                return (letter + secretKey) > 'z' ? ('a' - 1) + ((letter + secretKey) - 'z') : letter + secretKey;
+            }
         }
 
         public string Decrypt(string cipher)
         {
-            // if secret key is divisible by letter count in alphabet,
-            // then there is no shift
             if (secretKey == 0)
             {
                 return cipher;
             }
 
-            // convert cipher to char array
             char[] letters = cipher.ToCharArray();
 
             for (int i = 0; i < letters.Length; i++)
             {
-                // ignore if it's not a letter
-                if (!char.IsLetter(letters[i]))
+                if (IsNotLetter(letters[i]))
                 {
                     continue;
                 }
-
-                // take ascii code of letter
-                int letterCode = (int)letters[i];
-
-                // new ascii code
-                int newCode;
-
-                // check if letter is upper case
-                if (char.IsUpper(letters[i]))
-                {
-                    // calculate new ascii code:
-                    // if new ascii code is smaller than A's ascii code,
-                    // I subtract from A's ascii code the difference of given letter's code to secret key
-                    // and subtract that from ascii code which is one larger than Z's code
-                    // so I wouldn't ignore letter Z
-                    // otherwise, new ascii code is new letter
-                    newCode = (letterCode - secretKey) < (int)'A' ? ((int)'Z' + 1) - ((int)'A' - (letterCode - secretKey)) : letterCode - secretKey;
-                }
-                else
-                {
-                    // similar as above, just with lower case letters
-                    newCode = (letterCode - secretKey) < (int)'a' ? ((int)'z' + 1) - ((int)'a' - (letterCode - secretKey)) : letterCode - secretKey;
-                }
-                letters[i] = (char)newCode;
+                
+                letters[i] = (char)DecryptLetter(letters[i]);
             }
 
-            // return plain text
             return new string(letters);
+        }
+
+        private int DecryptLetter(char letter)
+        {
+            if (char.IsUpper(letter))
+            {
+                // calculate new ascii code:
+                // if new ascii code is smaller than A's ascii code,
+                // I subtract from A's ascii code the difference of given letter's code to secret key
+                // and subtract that from ascii code which is one larger than Z's code
+                // so I wouldn't ignore letter Z
+                // otherwise, new ascii code is new letter
+                return (letter - secretKey) < (int)'A' ? ((int)'Z' + 1) - ((int)'A' - (letter - secretKey)) : letter - secretKey;
+            }
+            else
+            {
+                // similar as above, just with lower case letters
+                return (letter - secretKey) < (int)'a' ? ((int)'z' + 1) - ((int)'a' - (letter - secretKey)) : letter - secretKey;
+            }
         }
     }
 }
